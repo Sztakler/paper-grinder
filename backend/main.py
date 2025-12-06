c2d91d26cd3c64ce5afe2d9b3d2ca73c5e59315d
Krystian Jasionek <krystian.jasionek@gmail.com>
2025-12-06 16:37:33 +0100

    add file uploading logic

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from pypdf import PdfReader
 
 app = FastAPI()
 
 @app.get("/")
 def hello():
     return {"msg": "Hello, there."}
+
+@app.post("/upload")
+async def upload_pdf(file: UploadFile = File(...)):
+    reader = PdfReader("homer-iliada.pdf")
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+
+    return {"text": text[:500]}
