f811337d35df7ab7499909b3af78ce5c14aaad73
Metarth <58264262+Sztakler@users.noreply.github.com>
2025-12-06 17:05:30 +0100

    add pdf loading and text extraction logic (#7)
    * add python-multipart to requirements.txt
    
    * add file uploading logic
    
    * add test pdf
    
    * import pdfplumber
    
    * use ocr processing to ensure legible text
    
    * add pdf2image and pytesseract packages

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from pdf2image import convert_from_path
+import pytesseract
 
 app = FastAPI()
 
 @app.get("/")
 def hello():
     return {"msg": "Hello, there."}
+
+@app.post("/upload")
+async def upload_pdf(file: UploadFile = File(...)):
+    pages = convert_from_path("homer-iliada.pdf")
+    text = ""
+    for page in pages[:5]:
+        text += pytesseract.image_to_string(page, lang="pol") + '\n'
+
+    return {"text": text[:1000]}
