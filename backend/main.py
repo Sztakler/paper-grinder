29f919c41aaadfb55cebbaae3385f38804f1b19c
Metarth <58264262+Sztakler@users.noreply.github.com>
2025-12-06 17:24:32 +0100

    pypdf to ocr fallback (#8)
    * fallback to ocr if pypdf results with illegible text
    
    * change fallback threshold

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, UploadFile, File
-from pdf2image import convert_from_path
+from pypdf import PdfReader
+from pdf2image import convert_from_bytes
 import pytesseract
+import re
 
 app = FastAPI()
 
@@ -8,11 +10,27 @@ app = FastAPI()
 def hello():
     return {"msg": "Hello, there."}
 
+def is_text_legible(text: str) -> bool:
+    alnum = len(re.findall(r"[a-zA-Z0-9żźćńąśęóŻŹĆŃĄŚŁĘÓ]", text))
+    total = len(text)
+    return total > 0 and (alnum / total) > 0.6
+
 @app.post("/upload")
 async def upload_pdf(file: UploadFile = File(...)):
-    pages = convert_from_path("homer-iliada.pdf")
+    
+    reader = PdfReader(file.file)
     text = ""
-    for page in pages[:5]:
-        text += pytesseract.image_to_string(page, lang="pol") + '\n'
+    for page in reader.pages[:5]:
+        page_text = page.extract_text()
+        text += page_text + '\n' if page_text else ""
+
+    if not is_text_legible(text):
+        file.file.seek(0)
+        content = file.file.read()
+        pages = convert_from_bytes(content)
+        text = ""
+        for page in pages[:5]:
+            text += pytesseract.image_to_string(page, lang="pol") + "\n"
 
     return {"text": text[:1000]}
+
