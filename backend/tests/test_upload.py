a2ce5e62f822895aa62c681bc8314031cbe4952f
Krystian Jasionek <krystian.jasionek@gmail.com>
2025-12-30 10:43:43 +0100

    add upload tests to backend

---
+++ backend/tests/test_upload.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_root():
+    root = client.get("/")
+    assert root.status_code == 200
+    assert root.json() == {"msg": "Hello, there."}
+
+def tets_upload_pdf():
+    fake_pdf = b"%PDF-1.4 fake pdf content"
+
+    files = {
+        "file": ("test.pdf", fake_pdf, "application/pdf")
+    }
+
+    root = client.post("/upload", files=files)
+    assert root.status_code == 200
+    assert "job_id" in root.json()
